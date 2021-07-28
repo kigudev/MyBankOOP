33ea0407bfa8cb71098cccdcee1aab6a61ffc303
Aleks Quiñones <kigu@monobits.mx>
2021-07-27 18:52:41 -0700

    Add project files.

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+        private List<Transaction> allTransactions { get; set; } = new List<Transaction>();
+
+        private static int accountNumberSeed = 1234567890;
+
+        public BankAccount(string name, decimal initialBalance) {
+            Owner = name;
+
+            MakeDeposit(initialBalance, DateTime.Now, "Balance inicial");
+
+            Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note) {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "La cantidad de depósito tiene que ser positivo");
+            }
+
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+        public void MakeWithdrawal(decimal amount, DateTime date, string note) {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "La cantidad de depósito tiene que ser positivo");
+            }
+            if(Balance - amount < 0)
+            {
+                throw new InvalidOperationException("No suficientes fondos en la cuenta");
+            }
+
+            var deposit = new Transaction(-amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public string GetAccountHistory()
+        {
+            var report = new StringBuilder();
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNotes");
+            foreach (var transaction in allTransactions)
+            {
+                balance += transaction.Amount;
+                report.AppendLine($"{transaction.Date.ToShortDateString()}\t{transaction.Amount}\t{balance}\t{transaction.Notes}");
+            }
+
+            return report.ToString();
+        }
+    }
+}
---
+++ BankAccount/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MyBank
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var account = new BankAccount("Rene", 1000);
+            Console.WriteLine($"la cuenta con número {account.Number} de {account.Owner} fue creada con {account.Balance} pesos");
+
+            try
+            {
+                account.MakeWithdrawal(200, DateTime.Now, "Para los tacos");
+                Console.WriteLine($"Nuevo balance: {account.Balance}");
+
+                account.MakeDeposit(100, DateTime.Now, "Regalo de amigo");
+                Console.WriteLine($"Nuevo balance: {account.Balance}");
+
+                account.MakeDeposit(-100, DateTime.Now, "Regalo de amigo 2");
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Se intentó agregar una cantidad con negativos");
+                Console.WriteLine(ex.Message);
+            }catch(InvalidOperationException ex)
+            {
+                Console.WriteLine("Se intentó sacar más dinero de lo disponible");
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                account.MakeWithdrawal(2000, DateTime.Now, "Para los tacos");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine("Se intentó sacar más dinero de lo disponible");
+                Console.WriteLine(ex.Message);
+            }
+
+            account.MakeDeposit(500, DateTime.Now, "Regalo de amigo 3");
+            Console.WriteLine($"Nuevo balance: {account.Balance}");
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+
+
+    }
+}
