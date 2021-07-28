dadbb39dbcbf71c18d8dec1b7358e237508c3e5d
Aleks Quiñones <kigu@monobits.mx>
2021-07-28 12:18:14 -0700

    limpia de código

--- BankAccount/InterestEarningAccount.cs
+++ BankAccount/InterestEarningAccount.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyBank
 {
@@ -10,12 +6,11 @@ namespace MyBank
     {
         public InterestEarningAccount(string name, decimal initialBalance) : base(name, initialBalance)
         {
-
         }
 
         public override void PerformMonthEndTransactions()
         {
-            if(Balance > 500m)
+            if (Balance > 500m)
             {
                 var interest = Balance * 0.05m;
                 MakeDeposit(interest, DateTime.Now, "Se dio interes mensual");
